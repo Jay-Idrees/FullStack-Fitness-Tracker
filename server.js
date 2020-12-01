24ccd680cbb22b6145faaa036741438d1f9458bf
Jay Idrees <idreesjay@gmail.com>
2020-11-30 22:56:48 -0500

    Configured server.js by specifying the dependencies express mongoose and morgan/logging info. Specified connection with the mongoose database

---
+++ server.js
@@ -0,0 +1,21 @@
+// Specifying dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// Specifying JSON parsing of data using Express routes
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Setting up the Mongoose conection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+// Listener
+app.listen(PORT, () => {
+    console.log("Welcome to Fitness Trainer, your server is running at http://localhost" +  PORT);
+  });
